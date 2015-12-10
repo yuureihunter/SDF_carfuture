d064a0222ab8281de14dd0debeefc380c7135834
pimpavee <58070101@kmitl.ac.th>
2015-12-10 14:14:24 +0700

    pull not complet ทำใหม่

--- SDF_car.py
+++ SDF_car.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 fig = plt.figure()
-ax1 = fig.add_subplot(211)
+ax1 = fig.add_subplot(111)
 fig.suptitle('Graph of stock car in september 2010 - 2015', fontsize=14, fontweight='bold')
 ax1.set_title('show stock car and future stock car')
 ax1.set_xlabel('year201X')
@@ -14,7 +14,6 @@ ax1.plot(x,y,c='#1C86EE',label='stock car')
 for x,y in zip(x,y):
     plt.text(x, y, '%.2f' % y, ha='left', va= 'bottom',\
     bbox={'facecolor':'#B0E0E6', 'alpha':0.5, 'pad':2.5})
-    
 w = [5,6]
 z = [3.65,3.7] #ค่าสุดท้ายยังไม่ใช่ค่าจริงๆ
 ax1.plot(w,z,c='#FF6347',label='future stock car')
@@ -22,25 +21,22 @@ for x,y in zip(w,z):
     plt.text(x, y, '%.2f' % y, ha='left', va= 'bottom',\
     bbox={'facecolor':'#FA8072', 'alpha':0.5, 'pad':2.5})
 
-
-ax2 = fig.add_subplot(212)
-ax2.set_title('show diff of car and diff future ')
-ax2.set_xlabel('year201X')
-ax2.set_ylabel('value(million)')
-
 a = [1,2,3,4,5]
 b = [1.7,1.8,2.4,1.3,0.9]
-ax2.plot(a,b,c='#32CD32',label='diff car')
+ax1.plot(a,b,c='#32CD32',label='diff car')
 for x,y in zip(a,b):
     plt.text(x, y, '%.1f' % y, ha='left', va= 'bottom',\
     bbox={'facecolor':'#54FF9F', 'alpha':0.5, 'pad':2.5})
-    
 g = [5,6]
 h = [0.9,1.5] #ค่าสุดท้ายยังไม่ใช่ค่าจริงๆ
-ax2.plot(g,h,c='#FF7F00',label='future diff car')
+ax1.plot(g,h,c='#FF7F00',label='future diff car')
 for x,y in zip(g,h):
     plt.text(x, y, '%.1f' % y, ha='left', va= 'bottom',\
     bbox={'facecolor':'#FF8C69', 'alpha':0.5, 'pad':2.5})
+
+leg = ax1.legend(loc='lower left')
+plt.show()
+
 
 
 leg = ax1.legend(loc='lower left')
